3c8163eeda2e6513831e66c246d385767e56e9d5
IshtiaqueNafis <nafu22@gmail.com>
2022-09-21 22:50:32 -0400

    cleaned up code

--- app/src/main/java/com/example/interactivestory/MainActivity.java
+++ app/src/main/java/com/example/interactivestory/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.interactivestory;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     //region member variable
@@ -27,22 +25,19 @@ public class MainActivity extends AppCompatActivity {
         nameField = findViewById(R.id.nameEditText);
         startButton = findViewById(R.id.startButton);
 
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String name = nameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
-                startStory(name);
+        startButton.setOnClickListener(view -> {
+            String name = nameField.getText().toString();
+            Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+            startStory(name);
 
-            }
         });
 
     }
 
     private void startStory(String name) {
         Intent intent = new Intent(this, StoryActivity.class);
-        Resources resources = getResources();
-        String key = resources.getString(R.string.key_name);
+
+        String key = getString(R.string.key_name);
         intent.putExtra(key, name);
         startActivity(intent);
     }
